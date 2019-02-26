8b2083aab4a14ba6fac315a089042ac860de18a5
Prometheus77 <aaronsama@gmail.com>
2019-02-26 16:47:22 -0700

    create ctrl_c

---
+++ R/ctrl_c.R
@@ -0,0 +1,11 @@
+#' @title Control-C
+#'
+#' @description
+#' Copies a data frame to the clipboard (in Windows) in a format suitable for copy-and-paste into a spreadsheet program such as Microsoft Excel.
+#'
+#' @param table A data table to be copied to the clipboard
+#'
+#' @export
+ctrl_c <- function(table, sep = "\t", row.names = FALSE, col.names = TRUE) {
+  write.table(table, "clipboard", sep = sep, row.names = row.names, col.names = col.names)
+}
